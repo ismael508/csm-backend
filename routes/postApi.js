9f081f333f6901708027eeba48ce58e16b0ad02f
Ismael <gingerindianpt@gmail.com>
2025-11-08 12:38:19 +0000

    forgot to require nodemailer

--- routes/postApi.js
+++ routes/postApi.js
@@ -1,5 +1,6 @@
-    const express = require('express');
+const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 const User = require('../models/UserModel');
 const PlayerData = require('../models/PlayerDataModel')
